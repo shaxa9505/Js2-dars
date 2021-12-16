88a9083ed85de06004a847947db00e52ed076cfa
shaxa9505 <87860218+shaxa9505@users.noreply.github.com>
2021-12-16 12:37:20 +0500

    js
    js

---
+++ Js 2-dars/script.js
@@ -0,0 +1,84 @@
+// 2-dars
+
+// let a = +prompt('birinchi sonni yozing')
+// let b = +prompt('ikkinchi sonni yozing')
+
+// if(a > b){
+//     alert(a + b)
+// }
+// else{
+//     alert(a * b)
+// }
+
+
+// let year = +prompt("Ikkinchi jahon urushi qachon bowlangan")
+// let res;
+
+// if(isNaN(year)){
+//     res = "harf emas son yozing"
+// }
+// else if(1945 > year){
+//     res = "Urush bowlanmadi gitler tayyorlanyapdi"
+// }
+// else if(1945 < year){
+//     res = "urush tugadi Gitler uldi"
+// }
+// else{
+//     res = "Qoyil siz topdingiz"
+// }
+
+// alert(res)
+
+// || bu yoki operatori bunda bittasi true busayam true qaytaradi
+// && bu va operatori bunda bittasi true busayam false qaytaradi
+// != bu emas operatori
+
+// let num1 = +prompt('birinchi sonni yozing')
+// let num2 = +prompt('ikkinchi sonni yozing')
+// let num3 = +prompt('uchinchi sonni yozing')
+// let res
+
+// if(num1 > num2 && num1 > num3){
+//     res = num1+ "bu eng kotta raqam"
+// }
+// else if(num2 > num1 && num2 > num3){
+//     res = num2 + "bu eng kotta raqam"
+// }
+// else{
+//     res = num3 + 'bu eng kotta raqam'
+// }
+
+// alert(res)
+
+// jsfiddle.net bu saytga kirip kurasan
+
+
+// let i = 1;
+
+// while(i < 10){
+//     document.write(i + "<br>");
+//     i++
+// }
+
+// for(let i = 0; i <= 10; i++){
+//     document.write(i + '<br>')
+// }
+
+
+// let num = +prompt("Sonni yozing")
+// let power = +prompt("Darajasini yozing")
+// let res = 1;
+
+// for(let i = 1; i <= power; i++){
+//     res *= num
+// }
+
+// alert(res)
+
+
+// var user = {
+//     name: "Salom",
+//     age: 19
+// }
+// user.name = "Shohrux"
+// console.log(user.name);
